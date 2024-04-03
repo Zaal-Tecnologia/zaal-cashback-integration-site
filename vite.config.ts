03028c8b07dbe5c44a299801776200385d98b14c
Desenvolvimento <desenvolvimento@zaal.com.br>
2024-04-03 14:42:27 -0300

    feat: remove proxy from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,36 @@ import react from '@vitejs/plugin-react'
 
 import path from 'node:path'
 
-export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), '')
+const defaultConfig = {
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+}
 
+export default defineConfig(({ mode, command }) => {
   const target =
     mode === 'development'
       ? 'http://zaal.no-ip.info:8083'
-      : mode === 'production'
-        ? 'http://zaal.no-ip.info:8083'
-        : 'http://zaal.no-ip.info:8083'
+      : 'http://zaal.no-ip.info:8083'
 
-  return {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-    server: {
-      port: 3000,
-      proxy: {
-        '/api': {
-          target,
-          changeOrigin: false,
-          secure: false,
+  if (command === 'serve') {
+    return {
+      server: {
+        port: 3000,
+        proxy: {
+          '/api': {
+            target,
+            changeOrigin: false,
+            secure: false,
+          },
         },
       },
-    },
-    plugins: [react()],
+      ...defaultConfig,
+    }
+  } else {
+    return defaultConfig
   }
 })
