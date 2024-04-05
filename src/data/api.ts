4361dc58ee93f386940a02284fb3795a0a381b9c
Desenvolvimento <desenvolvimento@zaal.com.br>
2024-04-05 11:54:15 -0300

    fix: no-cors

--- src/data/api.ts
+++ src/data/api.ts
@@ -10,13 +10,14 @@ export function api(path: string, init?: RequestInit) {
   // http://zaal.no-ip.info:8083/api/v1/
 
   const BASE_URL =
-    import.meta.env.MODE === 'development'
+    // import.meta.env.MODE === 'development'
+    import.meta.env.MODE === ''
       ? '/api/v1/'
       : 'https://1290-200-164-236-60.ngrok-free.app/api/v1/'
 
   return fetch(BASE_URL.concat(path), {
     ...init,
-    mode: 'no-cors',
+    // mode: 'no-cors',
     headers,
     referrerPolicy: 'no-referrer',
   })
