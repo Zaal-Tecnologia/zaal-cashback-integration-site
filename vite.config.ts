ffa7cce745df05e1f8bc62e851ee4f564dc8abb0
Desenvolvimento <desenvolvimento@zaal.com.br>
2024-04-08 08:49:18 -0300

    feat(netlify.toml): change redirect page

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
@@ -11,7 +11,7 @@ const defaultConfig = {
   },
 }
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // const env = loadEnv(mode, process.cwd(), '')
 
   // process.env = { ...process.env,  }
