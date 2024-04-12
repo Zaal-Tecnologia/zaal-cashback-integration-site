d936fb9bd6ac390d6b9ad5b0ac1c33a5136eec6b
Desenvolvimento <desenvolvimento@zaal.com.br>
2024-04-12 09:05:22 -0300

    fix: prod server

--- src/data/api.ts
+++ src/data/api.ts
@@ -7,11 +7,11 @@ export function api(path: string, init?: RequestInit) {
     Authorization: authorization ?? '',
   }
 
-  // const PROD_NGROK_SERVER = 'https://cc77-200-164-236-60.ngrok-free.app/api/v1/'
-  const PROD_SERVER = 'http://zaal.no-ip.info:8083/api/v1/'
+  const PROD_NGROK_SERVER = 'https://cc77-200-164-236-60.ngrok-free.app/api/v1/'
+  // const PROD_SERVER = 'http://zaal.no-ip.info:8083/api/v1/'
 
   const BASE_URL =
-    import.meta.env.MODE === 'development' ? '/api/v1/' : PROD_SERVER
+    import.meta.env.MODE === 'development' ? '/api/v1/' : PROD_NGROK_SERVER
 
   return fetch(BASE_URL.concat(path), {
     ...init,
