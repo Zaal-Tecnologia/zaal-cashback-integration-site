388caf78bfe2f85499b7903349526c1c0bfca1b4
Desenvolvimento <desenvolvimento@zaal.com.br>
2024-04-26 17:15:59 -0300

    feat(profile/theme): create profile theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { AppRouter } from './routes'
 import { TooltipProvider } from './components/ui/tooltip'
+import { useEffect } from 'react'
 
 export const client = new QueryClient()
 
 function App() {
+  useEffect(() => {
+    const theme = localStorage.getItem('@zaalcashback:theme')
+
+    const doc = document.documentElement
+
+    if (theme === 'dark') doc.classList.add('dark')
+  }, [])
+
   return (
     <QueryClientProvider client={client}>
       <TooltipProvider>
